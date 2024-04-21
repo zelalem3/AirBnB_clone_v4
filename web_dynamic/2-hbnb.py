47e65adb7d612cd25511be0a2359cbd7d1346071
zelalem3 <zgetnet24@example.com>
2024-04-21 01:50:20 -0700

    new branch

--- web_dynamic/2-hbnb.py
+++ web_dynamic/2-hbnb.py
@@ -40,6 +40,23 @@ def hbnb_filters(the_id=None):
                            amens=amens,
                            places=places,
                            users=users)
+@app.route('/')
+def hbnb_filters1(the_id=None):
+    """
+    handles request to custom template with states, cities & amentities
+    """
+    state_objs = storage.all('State').values()
+    states = dict([state.name, state] for state in state_objs)
+    amens = storage.all('Amenity').values()
+    places = storage.all('Place').values()
+    users = dict([user.id, "{} {}".format(user.first_name, user.last_name)]
+                 for user in storage.all('User').values())
+    return render_template('2-hbnb.html',
+                           cache_id=uuid.uuid4(),
+                           states=states,
+                           amens=amens,
+                           places=places,
+                           users=users)
 
 if __name__ == "__main__":
     """
